dfe44d8fa57a8a84dca418b49afff20a244aecf2
James <jamescarterbell@gmail.com>
2020-09-17 16:54:24 -0400

    Working on basic entity stuff, most of this will probably need changes later

--- SmolCommon/src/entity.rs
+++ SmolCommon/src/entity.rs
@@ -1,5 +1,3 @@
 use super::component::*;
 
-pub trait EntityCommon{
-    fn add_component<C: Component, CS: ComponentStorage<C>>(&mut self, comp: C, comp_storage: &mut CS);
-}
+pub trait EntityCommon: PartialEq + Eq{}
--- SmolHBSECS/src/lib.rs
+++ SmolHBSECS/src/lib.rs
@@ -2,14 +2,50 @@ pub mod component;
 
 use SmolCommon::entity::*;
 use SmolCommon::component::*;
+use std::collections::VecDeque;
 
+#[derive(Clone)]
 struct Entity{
     index: usize,
     generation: usize,
 }
 
-impl EntityCommon for Entity{
-    fn add_component<'a, C: Component, CS: ComponentStorage<C>>(&mut self, comp: C, comp_storage: &mut CS){
-        todo!()
+impl EntityCommon for Entity{}
+
+impl PartialEq for Entity {
+    fn eq(&self, other: &Self) -> bool {
+        self.index == other.index && self.generation == other.generation
+    }
+}
+impl Eq for Entity {}
+
+struct EntityStorage{
+    entities: Vec<Entity>,
+    empties: VecDeque<Entity>,
+}
+
+impl EntityStorage{
+    pub fn new() -> Self{
+        EntityStorage{
+            entities: Vec::new(),
+            empties: VecDeque::new(),
+        }
+    }
+
+    fn create_entity(&mut self) -> &Entity{
+        match self.empties.pop_front(){
+            Some(mut entity) => {
+                self.entities.get(entity.index).unwrap()
+            },
+            None => {
+                self.entities.push(Entity{index: self.entities.len(), generation: 0});
+                &self.entities[self.entities.len()-1]
+            },
+        }
+    }
+
+    fn delete_entity(&mut self, entity: &Entity){
+        self.entities[entity.index].generation += 1;
+        self.empties.push_back(entity.clone());
     }
 }
