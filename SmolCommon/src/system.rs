13ff52ad9c494648d3af8de902b20a27b1fe96c2
James <jamescarterbell@gmail.com>
2020-10-28 11:48:55 -0400

    I think this could fix it

--- SmolCommon/src/system.rs
+++ SmolCommon/src/system.rs
@@ -32,6 +32,18 @@ pub trait System<'d>{
     }
 }
 
+pub trait SystemRunner<'d>{
+    fn sys_run<'w: 'd, W: WorldCommon>(world: &'w W);
+}
+
+impl<'d, T, Q> SystemRunner<'d> for T
+    where T: System<'d, SystemData = Q>,
+          Q: SystemData<'d> + Sized{
+    fn sys_run<'w: 'd, W: WorldCommon>(world: &'w W){
+        Self::run(Self::get_system_data(world));
+    }
+}
+
 pub trait SystemData<'d>{
     fn get_data<'w: 'd, W: WorldCommon>(world: &'w W) -> Self;
     fn get_dep_vec<'w: 'd, W: WorldCommon>(world: &W) -> DepVec;
